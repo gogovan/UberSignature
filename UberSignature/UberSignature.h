8aa90d039968f79acdbef4d99270f90035b63b23
Keith Chan <keith.chan@gogotech.hk>
2020-05-21 01:21:36 +0800

    Carthage supported

---
+++ UberSignature/UberSignature.h
@@ -0,0 +1,19 @@
+//
+//  UberSignature.h
+//  UberSignature
+//
+//  Created by Keith Chan on 5/21/20.
+//  Copyright © 2020 com.ggx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UberSignature.
+FOUNDATION_EXPORT double UberSignatureVersionNumber;
+
+//! Project version string for UberSignature.
+FOUNDATION_EXPORT const unsigned char UberSignatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UberSignature/PublicHeader.h>
+
+
